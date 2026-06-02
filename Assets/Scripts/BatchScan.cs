bad4882b5f9a5e5336a995e61e10ccc386e9fc55
Panda248 <40081083+Panda248@users.noreply.github.com>
2026-06-02 15:13:50 -0400

    Add option to save to disk

--- Assets/Scripts/BatchScan.cs
+++ Assets/Scripts/BatchScan.cs
@@ -8,16 +8,32 @@ using UnityEngine;
 public class BatchScan : MonoBehaviour
 {
     public Transform environmentParent;
-    public void Scan()
+    public bool saveToDisk;
+    bool started;
+    public void Scan(bool saveToDisk)
     {
         Debug.Log($"Scanning {environmentParent.childCount} objects");
         SceneScanner.Instance().environmentParent = environmentParent;
-        SceneScanner.Instance().Scan();
+        SceneScanner.Instance().Scan(saveToDisk);
         for (int i = 0; i < environmentParent.childCount; i++)
         {
             ObjectScanner.Instance().ScanObject = environmentParent.GetChild(i).gameObject;
-            ObjectScanner.Instance().Scan();
+            ObjectScanner.Instance().Scan(saveToDisk);
         }
         ObjectScanner.Instance().ScanObject = null;
+    }
+
+    private void Awake()
+    {
+        started = true;
+    }
+
+    private void Update()
+    {
+        if(started) 
+        {
+            Scan(saveToDisk);
+            started = false;
+        }
     }
 }
